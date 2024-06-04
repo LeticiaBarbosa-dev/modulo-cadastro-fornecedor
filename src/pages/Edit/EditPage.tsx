45dc8da6c462ab73c1d3d7eaea5b7d6a8f723b70
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2024-06-04 16:04:37 -0300

    :lipstick: feat: Estilizando página de edição do item

--- src/pages/Edit/EditPage.tsx
+++ src/pages/Edit/EditPage.tsx
@@ -1,10 +1,13 @@
-
 import { SupplierForm } from "../../components/Form/SupplierForm";
+import "./style.css";
 
 export function EditPage() {
   return (
-    <div>
-      <h1>Editar Fornecedor</h1>
+    <div className="custom-edit-page">
+      <div>
+        <img src="/logo.svg" alt="" />
+        <h1>Editar Fornecedor</h1>
+      </div>
       <SupplierForm />
     </div>
   );
