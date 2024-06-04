e1747b44cf25e3b1b78a71e2a5a89414784f9c18
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2024-06-04 15:53:10 -0300

    :truck: chore: Renomeando classe de estilização da p[agina de cadastro

--- src/pages/Register/RegisterPage.tsx
+++ src/pages/Register/RegisterPage.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 
 export function RegisterPage() {
   return (
-    <div className="container-form">
+    <div className="custom-register-page">
       <div className="header">
         <img src="/logo.svg" alt="" />
         <h1>Cadastro de Fornecedor</h1>
