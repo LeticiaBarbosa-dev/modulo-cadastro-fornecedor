6340d64401bd183c20b256a9f2ef8275e84ecb58
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2024-06-05 18:09:59 -0300

    :recycle: refactor: Adicionando classe na página de edição

--- src/pages/Edit/EditPage.tsx
+++ src/pages/Edit/EditPage.tsx
@@ -4,7 +4,7 @@ import "./style.css";
 export function EditPage() {
   return (
     <div className="custom-edit-page">
-      <div>
+      <div className="header">
         <img src="/logo.svg" alt="" />
         <h1>Editar Fornecedor</h1>
       </div>
