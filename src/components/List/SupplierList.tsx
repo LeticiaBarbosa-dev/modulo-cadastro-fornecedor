f647bc09cf7181997c40699242ff2568c53d9434
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2024-06-05 18:09:04 -0300

    :recycle: refactor: Refatorando código do componente que lista os fornecedores

--- src/components/List/SupplierList.tsx
+++ src/components/List/SupplierList.tsx
@@ -1,33 +1,79 @@
-import { Button, List } from "antd";
-import { Supplier, useSuppliers } from "../../context/SupplierContext";
-import { Link } from "react-router-dom";
+import { useSuppliers } from "../../context/SupplierContext";
+import { Table } from "../Table/Table";
+import { TableCell } from "../Table/TableCell";
+import { TableHeader } from "../Table/TableHeader";
+import { TableRow } from "../Table/TableRow";
 import { EyeOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import { IconButton } from "../IconButton";
 import "./style.css";
 
 export function SupplierList() {
   const { suppliers } = useSuppliers();
-
   return (
-    <List className="container"
-      dataSource={suppliers}
-      renderItem={(item: Supplier) => (
-        <List.Item className="item"
-          actions={[
-            <div className="actions-container">
-              <Link to={`/fornecedor/${item.id}`} key="view">
-                <Button className="custom-view-button" type="default">
-                  <EyeOutlined />
-                </Button>
-              </Link>
-            </div>,
-          ]}
-        >
-          <List.Item.Meta className="item-meta"
-            title={item.responsavel}
-            description={`Email: ${item.email}, Telefone: ${item.telefone}`}
-          />
-        </List.Item>
-      )}
-    />
+    <Table>
+      <thead>
+        <tr className="header-table">
+          <TableHeader>CNPJ</TableHeader>
+          <TableHeader>Razão Social</TableHeader>
+          <TableHeader>Nome Fantasia</TableHeader>
+          <TableHeader>Telefone</TableHeader>
+          <TableHeader style={{ width: 64 }}></TableHeader>
+        </tr>
+      </thead>
+      <tbody>
+        {suppliers.map((supplier) => {
+          return (
+            <TableRow key={supplier.id}>
+              <TableCell>{supplier.cnpj}</TableCell>
+              <TableCell>{supplier.razao_social}</TableCell>
+              <TableCell>{supplier.nome_fantasia}</TableCell>
+              <TableCell>{supplier.telefone}</TableCell>
+              <TableCell id="button-view-cell">
+                <Link to={`/fornecedor/${supplier.id}`} key="view">
+                  <IconButton className="custom-view-button">
+                    <EyeOutlined />
+                  </IconButton>
+                </Link>
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </tbody>
+      {/* <tfoot>
+        <tr>
+          <TableCell colSpan={3}>
+            Mostrando {attendees.length} de {total} itens
+          </TableCell>
+          <TableCell className="text-right" colSpan={3}>
+            <div className="inline-flex items-center gap-8">
+              <span>
+                Página {page} de {totalPages}
+              </span>
+              <div className="flex gap-1.5">
+                <IconButton onClick={goToFirstPage} disabled={page === 1}>
+                  <ChevronsLeft className="size-4" />
+                </IconButton>
+                <IconButton onClick={goToPreviousPage} disabled={page === 1}>
+                  <ChevronLeft className="size-4" />
+                </IconButton>
+                <IconButton
+                  onClick={goToNextPage}
+                  disabled={page === totalPages}
+                >
+                  <ChevronRight className="size-4" />
+                </IconButton>
+                <IconButton
+                  onClick={goToLastPage}
+                  disabled={page === totalPages}
+                >
+                  <ChevronsRight className="size-4" />
+                </IconButton>
+              </div>
+            </div>
+          </TableCell>
+        </tr>
+      </tfoot> */}
+    </Table>
   );
 }
